d61ccf0c6fe66ec98ce975da1d1eccd5b8ebb583
artem <77747763+jscelle@users.noreply.github.com>
2023-01-24 14:00:56 +0300

    Update practice6_1-3.cpp

--- practice6_1-3.cpp
+++ practice6_1-3.cpp
@@ -1,128 +1,151 @@
 #include <iostream>
-#include <algorithm>
-#include <Windows.h>
-
+#include <vector>
+#include <cmath>
+#include <string>
 using namespace std;
 
-/*
-    Sets the cursor to specified (x,y) position in console
-    params:
-    x - x position of the cursor
-    y - y position of the cursor
-*/
-void gotoxy(int x, int y) {
-    COORD pos = { x, y };
-    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(output, pos);
-}
-
-struct node {
-    int key;
-    node* left;
-    node* right;
+struct tree {
+	int data;
+	tree* left;
+	tree* right;
 };
 
-class binary_tree {
-private:
-    node* root = 0;
-public:
-    node* get_root() {
-        return root;
-    }
-    void insert(const int key, const int value) {
-        if (!root) {
-            root = new node{ key,0,0 };
-            root->left = new node{ value,0,0 };
-            return;
-        }
-        node* n = find(root, key);
-        if (!n) return;
-        if (!n->left) {
-            n->left = new node{ value,0,0 };
-            return;
-        }
-        n->right = new node{ value,0,0 };
-    }
-    node* find(node* n, const int key) {
-        if (!root || !n) return 0;
-        if (n->key == key) return n;
-        node* r1 = find(n->left, key);
-        if (r1) return r1;
-        node* r2 = find(n->right, key);
-        return r2;
-    }
-    ~binary_tree() {
-        clear_nodes(root);
-    }
-private:
-    void clear_nodes(node* node) {
-        if (node) {
-            clear_nodes(node->left);
-            clear_nodes(node->right);
-            delete node;
-        }
-    }
-};
-
-/*
-   Returns the depth of the binary tree
-   params:
-   root - pointer to root of the binary tree
-   lvl - current level of the binary tree (by default lvl = 0)
-*/
-int get_tree_level(node* root, const int lvl = 0) {
-    if (!root) return lvl;
-    int l1 = get_tree_level(root->left, lvl + 1);
-    int l2 = get_tree_level(root->right, lvl + 1);
-    return max(l1, l2);
+struct tree* addnd(int x, int y, tree* tr) {
+	tr = new tree; // Ô‡ÏˇÚ¸ ÔÓ‰ ÛÁÂÎ
+	tr->data = x;   // ÔÓÎÂ ‰‡ÌÌ˚ı
+	tr->left = new tree;
+	tr->left->left = NULL;
+	tr->left->right = NULL;
+	tr->left->data = y;
+	tr->right = NULL; // ‚ÂÚ‚Ë ËÌËˆË‡ÎËÁËÛÂÏ ÔÛÒÚÓÚÓÈ
+	return(tr);
 }
 
-/*
-    Prints node's key and its left and right branches
-    params:
-    n - pointer to node that we need to print
-    x - x position of cursor in console
-    y - y position of cursor in console
-    h - height of left and right branches
-*/
-void print_node(node* n, const int x, const int y, const int h) {
-    if (!n) return;
-    gotoxy(x, y); cout << n->key;
-    for (int i = 1; i <= h; ++i) {
-        if (n->left) { gotoxy(x - i, y + i); cout << '/'; }
-        if (n->right) { gotoxy(x + i, y + i); cout << "\\"; }
-    }
-    if (n->left) print_node(n->left, x - h - 1, y + h + 1, (h - 1) >> 1);
-    if (n->right) print_node(n->right, x + h + 1, y + h + 1, (h - 1) >> 1);
+struct tree* addnode(int x, int y, tree* tr) {
+	if (tr != NULL)
+	{
+		if (x == tr->data)
+		{
+			if (tr->left == NULL)
+			{
+				tr->left = new tree;
+				tr->left->data = y;
+				tr->left->left = NULL;
+				tr->left->right = NULL;
+			}
+			else
+			{
+				tr->right = new tree;
+				tr->right->data = y;
+				tr->right->left = NULL;
+				tr->right->right = NULL;
+			}
+		}
+		else
+		{
+			if (tr->left)
+				addnode(x, y, tr->left);
+			if (tr->right)
+				addnode(x, y, tr->right);
+		}
+	}
+	return(tr);
 }
 
-/*
-    Prints binary tree node by node
-    params:
-    root - root node of the binary tree
-*/
-void print_binary_tree(node* root,const int startY) {
-    int lvl = get_tree_level(root);
-    int h = 1;
-    for (int i = 0; i < lvl - 2; ++i) h = (h << 1) + 1;
-    print_node(root, 20, startY, h);
+
+void printLevel(tree* t, int level, int prbl) {
+	if (t == nullptr) {
+		// ≈ÒÎË ‰ÂÂ‚Ó ÔÛÒÚÓÂ, ÚÓ ÓÚÓ·‡Ê‡Ú¸ ÌÂ˜Â„Ó, ‚˚ıÓ‰ËÏ
+		return;
+	}
+	else {
+		printLevel(t->left, level - 1, (prbl / 2));  // — ÔÓÏÓ˘¸˛ ÂÍÛÒËË ÔÓÒÂ˘‡ÂÏ ÎÂ‚ÓÂ ÔÓ‰‰ÂÂ‚Ó
+		if (level == 0) {
+			// level ·Û‰ÂÚ ‡‚ÂÌ ÌÛÎ˛ Ì‡ ÌÛÊÌÓÈ „ÎÛ·ËÌÂ, Ú‡Í Í‡Í ÔË Í‡Ê‰ÓÏ ÂÍÛÒË‚ÌÓÏ ‚˚ÁÓ‚Â ÁÌ‡˜ÂÌËÂ level ÛÏÂÌ¸¯‡ÂÚÒˇ Ì‡ Ó‰ËÌ
+			for (int o = 0; o < prbl - 1; o++)
+				cout << " ";
+			cout << t->data << " ";  // œÓÍ‡Á˚‚‡ÂÏ ˝ÎÂÏÂÌÚ, ÂÒÎË ÓÌ Ì‡ ÌÛÊÌÓÏ Ì‡Ï ÛÓ‚ÌÂ
+			for (int o = 0; o < prbl - 1; o++)
+				cout << " ";
+		}
+		printLevel(t->right, level - 1, (prbl / 2));  // — ÔÓÏÓ˘¸˛ ÂÍÛÒËË ÔÓÒÂ˘‡ÂÏ Ô‡‚ÓÂ ÔÓ‰‰ÂÂ‚Ó
+	}
 }
 
-int main() {
-
-    binary_tree tree;
-
-    cout << "Enter number of vertexes : ";
-    int count; cin >> count;
-
-    for (int i = 0; i < count - 1; ++i) {
-        cout << "(key,value) : ";
-        int key; cin >> key;
-        int value; cin >> value;
-        tree.insert(key, value);
-    }
-
-    print_binary_tree(tree.get_root(),count + 1);
-
-    return 0;
+bool proverka(tree* t) {
+	bool ae = true, ae1 = true, ae2 = true;
+	if (t->left->data > t->right->data)
+	{
+		ae = false;
+	}
+	else
+	{
+		if (t->left->left && t->left->right) ae1 = proverka(t->left);
+		if (t->right->left && t->right->right) ae2 = proverka(t->right);
+		ae = ae1 && ae2;
+	}
+	return ae;
 }
+
+int poisk(tree* t, int sm, int pp, bool* a)
+{
+	pp = pp * 10 + t->data;
+	if (sm == t->data)
+	{
+		*a = true;
+	}
+	else if ((sm < t->data) && (t->left != NULL))
+	{
+		pp = poisk(t->left, sm, pp, a);
+	}
+	if ((sm > t->data) && (t->left != NULL))
+	{
+		pp = poisk(t->right, sm, pp, a);
+	}
+	return pp;
+}
+
+int main()
+{
+	int N, a, b, ct = 1, lim = 1, prbl, psk, o, pp = 0;
+	string put;
+	struct tree* derevo;
+	derevo = NULL;
+	bool prvvrk = false;
+	cout << "Enter N\n";
+	cin >> N;
+	cout << "Enter info about tree\n";
+	cin >> a >> b;
+	derevo = addnd(a, b, derevo);
+	for (int i = 0; i < N - 2; i++)
+	{
+		cin >> a >> b;
+		derevo = addnode(a, b, derevo);
+	};
+	while (lim < N)
+	{
+		ct++;
+		lim *= 2;
+	}
+	//	demonstration(derevo, ct);
+	cout << endl;
+	prbl = (pow(2, (ct - 1))) / 2;
+	for (int p = 0; p < ct; p++)
+	{
+		printLevel(derevo, p, prbl);
+		cout << endl;
+	}
+	cout << endl;
+	if (proverka(derevo))
+		cout << "True tree";
+	else
+		cout << "False tree";
+	cout << endl;
+	cin >> o;
+	pp = poisk(derevo, o, pp, &prvvrk);
+	if (prvvrk)
+		cout << pp;
+	else
+		cout << "Element not found";
+}
+
